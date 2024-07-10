049571f91b1e4ea54210cf7970929f4e281cae3c
zueunjoeng <judms0314@gmail.com>
2024-07-10 09:53:43 +0900

    변수지정해서 빠르게 db가 자리로 찾아갈 수 있도록한다.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import './App.css';
 import datainfo from './data/db.json';
 import Mainbanner from './Banner';
+import Text from "./text"
+
 
 function App() {
   return (
-    <div className="App">
-      <ul>
-      {
-        datainfo["gnb"].map((e, i)=>{
-          
-          return(
-          <li>
-            <a href={e.href}>
-              {e.atext}
-            </a>
-          </li>)
-
-        })
-      }
-      </ul>
-      <Mainbanner></Mainbanner>
+    <div>
+      <Text dbinfo = {datainfo["gnb"][0]}></Text>
+      <Text dbinfo = {datainfo["gnb"][1]}></Text>
+      <Text dbinfo = {datainfo["gnb"][2]}></Text>
     </div>
   );
 }
+//재사용이 편리하다. 정확한 집으로 갈 수 있도록
+//dbinfo는 변수여서 내가 마음대로 지어도 된다. 
+//["gnb"][]에 text.js에서 적은 props가 정확히 갈 수 있도록
+//atext가 ["gnb"][i] 순서대로 맞게 갈 수 있도록.
 
 export default App;
---
+++ src/text.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Text(props) {
+    return (
+        <div>{props.dbinfo.atext}</div>
+    )
+}
+
+export default Text
